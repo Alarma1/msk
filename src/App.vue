263278a9cf1647c8fcc804a53969ea10586d979e
Alarma1 <zapara03@yandex.ru>
2023-10-16 19:41:39 +0500

    Сделал логику форм и кнопки

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mainComponent/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import mainComponent from '@/components/MainComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    mainComponent,
   },
 };
 </script>
